98eccb524706f26d6b9b4a805e7f44c0b4eeccda
Ayushmangiri <ayushmangiri@gmail.com>
2025-06-11 00:18:22 +0530

    11/06/2025

--- src/main/java/com/example/demo/controller/UserService.java
+++ src/main/java/com/example/demo/controller/UserService.java
@@ -4,9 +4,24 @@ import com.example.demo.models.User;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 
 public class UserService {
-    private List<User> store = new ArrayList<>();
+    private final List<User>store= new ArrayList<>();
+
+    public UserService(){
+        store.add(new User(UUID.randomUUID().toString(),"Ayushmna","ayushmangiri@gmail.com","pass@123"));
+        store.add(new User(UUID.randomUUID().toString(),"harsh","harshgiri@gmail.com","pass@123"));
+
+        store.add(new User(UUID.randomUUID().toString(),"akshay","akshaygiri@gmail.com","pass@123"));
+
+        store.add(new User(UUID.randomUUID().toString(),"chandra","chandragiri@gmail.com","pass@123"));
+
+    }
+    public List<User> getUsers(){
+        return this.store;
+    }
 }
--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -1,4 +1,16 @@
 package com.example.demo.models;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
 public class User {
+    private String userId;
+    private String name;
+    private String email;
+    private String Password;
 }
