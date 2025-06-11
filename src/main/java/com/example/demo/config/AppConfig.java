444e7eb03228a047ec988ebbb220f7a78437d8c2
Ayushmangiri <ayushmangiri@gmail.com>
2025-06-12 01:30:27 +0530

    added login details

--- src/main/java/com/example/demo/config/AppConfig.java
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -1,13 +1,36 @@
 package com.example.demo.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class AppConfig {
-    public UserDetailsService userDetailsService{
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user = User.builder().username("Ayush").password(passwordEncoder().encode("Ayushman")).roles("Admin").build();
+        UserDetails user1 = User.builder().username("Akshay").password(passwordEncoder().encode("Chadani")).roles("Admin").build();
+
+        return new InMemoryUserDetailsManager(user,user1);
 
     }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+//    @Bean
+//    public Authentication authentication(AuthenticationConfiguration getAuthenticationConfiguration){
+//        return  builder.getA
+//    }
 
 }
