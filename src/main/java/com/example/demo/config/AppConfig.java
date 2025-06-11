1e3c869399ca97c44145c0d8f8ffd2f6bc3511db
Ayushmangiri <ayushmangiri@gmail.com>
2025-06-11 18:53:22 +0530

    Added

---
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.demo.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+public class AppConfig {
+    public UserDetailsService userDetailsService{
+
+    }
+
+}
